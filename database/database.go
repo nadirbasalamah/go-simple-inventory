f1879509a706d2582e1a25265e3fbb3bb6487f87
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-14 13:31:59 +0700

    feat(db): Adding connection to the database. Adding connection to the MySQL database.

---
+++ database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/nadirbasalamah/go-simple-inventory/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+
+	var (
+		databaseUser     string = utils.GetValue("DB_USER")
+		databasePassword string = utils.GetValue("DB_PASSWORD")
+		databaseHost     string = utils.GetValue("DB_HOST")
+		databasePort     string = utils.GetValue("DB_PORT")
+		databaseName     string = utils.GetValue("DB_NAME")
+	)
+
+	var dataSource string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", databaseUser, databasePassword, databaseHost, databasePort, databaseName)
+	var err error
+
+	DB, err = gorm.Open(mysql.Open(dataSource), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Connected to the database")
+}
--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/nadirbasalamah/go-simple-inventory/database"
 	"github.com/nadirbasalamah/go-simple-inventory/routes"
 )
 
 func main() {
 	var app *fiber.App = fiber.New()
+
+	database.InitDatabase()
 
 	routes.SetupRoutes(app)
 
