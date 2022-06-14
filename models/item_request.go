edce9cb8641b48cd45c9e7d076b3f63aa3047b55
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-14 14:15:01 +0700

    feat(helper): Adding helper for getting validation error message. Adding helper for getting validation error message for item request.

--- models/item_request.go
+++ models/item_request.go
@@ -23,16 +23,3 @@ func (itemInput ItemRequest) ValidateStruct() []*ErrorResponse {
 
 	return errors
 }
-
-func getErrorMessage(err validator.FieldError) string {
-	switch err.Tag() {
-	case "required":
-		return err.Field() + " is required"
-	case "gt":
-		return "the value of " + err.Field() + " must be greater than " + err.Param()
-	case "gte":
-		return "the value of " + err.Field() + " must be greater than or equals " + err.Param()
-	default:
-		return "validation error in " + err.Field()
-	}
-}
