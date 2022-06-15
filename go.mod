f86cee7e8cdae905b941e4a765ccb7fdef46bea2
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-15 09:49:55 +0700

    feat(service): Adding implementation with MySQL in service. Adding implementation with MySQL Database in service component.

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.18
 
 require (
 	github.com/go-playground/validator/v10 v10.11.0
-	github.com/gofiber/fiber/v2 v2.34.0
+	github.com/gofiber/fiber/v2 v2.34.1
+	github.com/gofiber/jwt/v3 v3.2.12
 	github.com/golang-jwt/jwt/v4 v4.4.1
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
@@ -25,6 +26,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.37.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
+	golang.org/x/sys v0.0.0-20220614162138-6c1b26c55098 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -3,15 +3,18 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/nadirbasalamah/go-simple-inventory/handlers"
+	"github.com/nadirbasalamah/go-simple-inventory/middlewares"
 )
 
 func SetupRoutes(app *fiber.App) {
+	// public routes
 	app.Post("/api/v1/signup", handlers.Signup)
 	app.Post("/api/v1/login", handlers.Login)
-
 	app.Get("/api/v1/items", handlers.GetAllItems)
 	app.Get("/api/v1/items/:id", handlers.GetItemByID)
-	app.Post("/api/v1/items", handlers.CreateItem)
-	app.Put("/api/v1/items/:id", handlers.UpdateItem)
-	app.Delete("/api/v1/items/:id", handlers.DeleteItem)
+
+	// private routes, authentication is required
+	app.Post("/api/v1/items", middlewares.CreateMiddleware(), handlers.CreateItem)
+	app.Put("/api/v1/items/:id", middlewares.CreateMiddleware(), handlers.UpdateItem)
+	app.Delete("/api/v1/items/:id", middlewares.CreateMiddleware(), handlers.DeleteItem)
 }
