87f35706f4a266d9b47558e8eda9e1461a90f57a
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-20 15:59:07 +0700

    fix(faker): Adding faker tags to the models. Adding faker tags to the models instead of adding to the new struct.

--- utils/faker.go
+++ utils/faker.go
@@ -1,55 +1,26 @@
 package utils
 
 import (
-	"time"
-
 	"github.com/bxcodec/faker/v3"
 	"github.com/nadirbasalamah/go-simple-inventory/models"
 )
 
 func CreateItemFaker() (models.Item, error) {
-	type ItemData struct {
-		ID       string `faker:"uuid_hyphenated"`
-		Name     string `faker:"name"`
-		Price    int    `faker:"oneof: 15, 27, 61"`
-		Quantity int    `faker:"oneof: 15, 27, 61"`
-	}
-
-	itemData := ItemData{}
-	err := faker.FakeData(&itemData)
+	var item models.Item = models.Item{}
+	err := faker.FakeData(&item)
 	if err != nil {
 		return models.Item{}, err
 	}
 
-	var newItem models.Item = models.Item{
-		ID:        itemData.ID,
-		Name:      itemData.Name,
-		Price:     itemData.Price,
-		Quantity:  itemData.Quantity,
-		CreatedAt: time.Now(),
-	}
-
-	return newItem, nil
+	return item, nil
 }
 
 func CreateUserFaker() (models.User, error) {
-	type UserData struct {
-		ID       string `faker:"uuid_hyphenated"`
-		Email    string `faker:"email"`
-		Password string `faker:"password"`
-	}
-
-	userData := UserData{}
-	err := faker.FakeData(&userData)
+	var user models.User = models.User{}
+	err := faker.FakeData(&user)
 	if err != nil {
 		return models.User{}, err
 	}
 
-	var newUser models.User = models.User{
-		ID:       userData.ID,
-		Email:    userData.Email,
-		Password: userData.Password,
-	}
-
-	return newUser, nil
+	return user, nil
 }
