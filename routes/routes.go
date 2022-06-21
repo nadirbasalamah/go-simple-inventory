2c862528e9e4eff874d7759ebffd0c18b39d2bbc
nadirbasalamah <nadir008basalamah@gmail.com>
2022-06-21 16:35:42 +0700

    fix(routes): Remove unused route. Remove unused route in routes file.

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,6 @@
 package routes
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/nadirbasalamah/go-simple-inventory/handlers"
 	"github.com/nadirbasalamah/go-simple-inventory/middlewares"
@@ -20,23 +17,4 @@ func SetupRoutes(app *fiber.App) {
 	app.Post("/api/v1/items", middlewares.CreateMiddleware(), handlers.CreateItem)
 	app.Put("/api/v1/items/:id", middlewares.CreateMiddleware(), handlers.UpdateItem)
 	app.Delete("/api/v1/items/:id", middlewares.CreateMiddleware(), handlers.DeleteItem)
-
-	// testing only
-	app.Get("/user/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-		if id == "" {
-			return c.SendStatus(http.StatusBadRequest)
-		}
-
-		if id == "1234" {
-			c.Cookie(&fiber.Cookie{
-				Name:    "CookieForAndy",
-				Value:   "Andy",
-				Expires: time.Now().Add(24 * time.Hour),
-			})
-			return c.JSON(map[string]string{"id": "1234", "name": "Andy"})
-		}
-
-		return c.SendStatus(http.StatusNotFound)
-	})
 }
